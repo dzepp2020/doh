21ae1f8fdffa643d227dff622a33ad3061b7cdb2
dzepp2020 <dzepp1@icloud.com>
2026-06-15 10:20:42 -0400

    Add recordType dictionary for DNS record types
    Updated a comment.

--- dns.py
+++ dns.py
@@ -5,6 +5,7 @@ import argparse
 
 #
 # Global variables
+#
 recordType={
 1:'A', 2:'NS', 3:'MD', 4:'MF', 5:'CNAME', 6:'SOA', 7:'MB', 8:'MG', 9:'MR', 10:'NULL', 11:'WKS', 12:'PTR',
 13:'HINFO', 14:'MINFO', 15:'MX', 16:'TXT', 17:'RP', 18:'AFSDB', 19:'X25', 20:'ISDN', 21:'RT', 22:'NSAP',
